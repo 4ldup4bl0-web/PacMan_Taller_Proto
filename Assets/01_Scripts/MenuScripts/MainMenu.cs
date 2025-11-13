7d8d4cf3e56194fc42f5b8d16e3daf33876bc369
gold <gold39954@gmail.com>
2025-11-13 17:29:35 -0300

    :v

---
+++ Assets/01_Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject OptionsMenu;
+    public GameObject Mainmenu;
+
+    private void Start()
+    {
+        Mainmenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+
+    }
+
+    public void OpenOptionsPanel()
+    {
+        Mainmenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+
+    public void OpenMainMenuPanel()
+    {
+        Mainmenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
