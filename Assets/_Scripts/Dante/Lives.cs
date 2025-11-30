9dbebf7d859cfa092ddffb57c01bfd4a54b33b7e
WDGH-TL <williamdantegarcia@gmail.com>
2025-11-30 01:11:52 -0300

    Lives work now

--- Assets/_Scripts/Dante/Lives.cs
+++ Assets/_Scripts/Dante/Lives.cs
@@ -7,13 +7,44 @@ using System.Collections.Generic;
 public class Lives : MonoBehaviour
 {
     public List<Image> lives;
+    private int currentLives;
 
-    void OnCollisionEnter(Collision collision)
+    void Start()
+    {
+        currentLives = lives.Count;
+        refreshUI();
+    }
+
+    public void refreshUI()
+    {
+        for (int i = 0; i < lives.Count; i++)
+        {
+            if(i < currentLives)
+            {
+                lives[i].enabled = true;
+            }
+            else
+            {
+                lives[i].enabled = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy"))
         {
-            Debug.Log("Enemy collisioned");
+            if(currentLives > 0)
+            {
+                currentLives--;
+                refreshUI();
+            }
+            if (currentLives <= 0)
+            {
+                Debug.Log("You Loose");
+                //Aqui hay que llamar a la función para cuando pierde el jugador
+            }
         }
 
     }
